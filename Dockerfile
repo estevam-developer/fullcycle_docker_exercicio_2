e4d5936453e45f03b5c39c28d1779183c6869865
Leonardo Alessandro Estevam <estevam.developer@gmail.com>
2022-05-02 15:45:39 -0300

    Adição de novo comportamento com uso de um segundo container para acesso ao mysql, usando o docker-compose para subir o ambiente. Nesta versão do exemplo o Node irá se comunicar com o Mysql, cada um em um container distinto

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,15 @@
-FROM node:15
+FROM node:15 as builder
 
 WORKDIR /usr/src/app
 
+RUN npm install && npm install mysql --save
+
 COPY src .
+
+FROM node:15-alpine
+
+WORKDIR /usr/src/app
+
+COPY --from=builder /usr/src/app .
 
 CMD [ "node", "index.js" ]
--- src/index.js
+++ src/index.js
@@ -2,7 +2,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const config = {
+    host: 'mysql-db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+}
+
+const mysql = require('mysql')
+
+const sql = `INSERT INTO people(name) VALUES ('Leonardo Alessandro Estevam')`
+
 app.get('/', (req, res) => {
+    const connection = mysql.createConnection(config)
+    connection.query(sql)
+    connection.end()
 
     res.send('<h1>Full Cycle!</h1>')
 })
